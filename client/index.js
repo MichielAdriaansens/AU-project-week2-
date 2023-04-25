ad564a9eb18745c7cdc9d52be9ee4b0ea05ec03a
MichielAdriaansens <m.adriaansens@gmail.com>
2023-04-25 15:38:34 +0200

    Alchemy University week2 assignment

--- client/index.js
+++ client/index.js
@@ -2,6 +2,11 @@ const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
+let input = process.argv[2];
+if (process.argv[3]) {
+  input = `${process.argv[2]} ${process.argv[3]}`;
+}
+
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
@@ -9,6 +14,7 @@ async function main() {
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name: input,
   });
 
   console.log({ gift });
